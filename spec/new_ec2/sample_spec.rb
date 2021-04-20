7c2f85695fe971970724972b0731e1250d652a7c
Hirotoshi-Kubo <77191463+Hirotoshi-Kubo@users.noreply.github.com>
2021-04-20 16:58:51 +0900

    Update sample_spec.rb

--- spec/new_ec2/sample_spec.rb
+++ spec/new_ec2/sample_spec.rb
@@ -23,9 +23,9 @@ describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
   it { should be_running }
 end
 
-describe port(80) do
-  it { should be_listening }
-end
+#describe port(80) do
+#  it { should be_listening }
+#end
 
 if os[:family] == 'amazon'
 
